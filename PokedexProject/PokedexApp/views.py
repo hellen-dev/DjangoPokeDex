ea93e79f8d11525f63a4ece2b66ef9b460e447d7
hellen-dev <rm98284@fiap.com.br>
2024-02-02 12:49:53 -0300

    modularizing for future views

--- PokedexProject/PokedexApp/views.py
+++ PokedexProject/PokedexApp/views.py
@@ -1,3 +1,51 @@
 from django.shortcuts import render
-
+from .models import Pokemon
+import requests
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
+
+# Send an HTTP GET request to the API and retrieve data in JSON
+def fetch_pokemon_data(pokemon_id):
+    response = requests.get(f'https://pokeapi.co/api/v2/pokemon/{pokemon_id}/')
+    data = response.json()
+    return data
+
+# Get or create a Pokemon instance in the database
+def create_or_get_pokemon(data):
+
+
+    # Extract types from the provided data
+    types = [type_info['type']['name'] for type_info in data['types']]
+
+    # Set sprite URL, defaulting to an empty string if not present
+    sprite_url = data['sprites']['front_default'] if 'sprites' in data and 'front_default' in data['sprites'] else ''
+
+    pokemon_instance, created = Pokemon.objects.get_or_create(
+        id=data['id'],
+        defaults={
+            'name': data['name'],
+            'height': data['height'],
+            'weight': data['weight'],
+            'is_default': True,
+            'sprite_url': sprite_url,
+        }
+    )
+
+    pokemon_instance.set_types(types)
+
+    return pokemon_instance, sprite_url
+    
+# Create and return a formatted dictionary containing Pokemon information   
+def format_pokemon_data(pokemon_instance, sprite_url):
+
+    type_list = pokemon_instance.get_types()
+
+    return {
+        'id': pokemon_instance.id,
+        'name': pokemon_instance.name,
+        'type': type_list,
+        'height': pokemon_instance.height_in_meters(),
+        'weight': pokemon_instance.weight_in_kilograms(),
+        'is_default': pokemon_instance.is_default,
+        'sprite_url': sprite_url,
+    }
