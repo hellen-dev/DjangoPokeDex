f520dcb3be62002348107d7bcf1d9f8976ed2b41
hellen-dev <rm98284@fiap.com.br>
2024-02-02 13:09:04 -0300

    Creating front-end for 'pokemon by weight' view and  refactoring pokemon class methods for better data handling and conversion

--- PokedexProject/PokedexApp/models.py
+++ PokedexProject/PokedexApp/models.py
@@ -11,5 +11,22 @@ class Pokemon(models.Model):
     is_default = models.BooleanField(default=True)
     sprite_url = models.URLField(default='')
 
+
+    def set_types(self, types):
+        # Set the 'type' attribute by joining the provided list of types
+        self.type = ', '.join(types)
+
+    def get_types(self):
+        # Get a list of types by splitting the 'type' attribute
+        return [t.strip() for t in self.type.split(',') if t.strip()]
+    
+    def height_in_meters(self):
+        # Convert height from decimeters to meters
+        return self.height / 10.0
+
+    def weight_in_kilograms(self):
+        # Convert weight from decigrams to kilograms
+        return self.weight / 10.0
+    
     def __str__(self):
         return self.name
